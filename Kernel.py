d4f39fc5b058dd71e6bd535d7476bba5d14d68c8
gupta-ujjwal <gupta.ujjwal14@gmail.com>
2019-03-09 23:32:23 +0530

    Added Resampling and feature scaling

--- Kernel.py
+++ Kernel.py
@@ -7,10 +7,13 @@ Created on Thu Mar  7 19:01:07 2019
 
 import pandas as pd
 import numpy as np
-
+from boruta import BorutaPy
 from sklearn import metrics
 import matplotlib.pyplot as plt
-
+from imblearn.over_sampling import SMOTE
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import chi2
+from sklearn import preprocessing
 
 train = pd.read_csv("DataBase/train.csv", header='infer')
 test = pd.read_csv("DataBase/test.csv", header='infer')
@@ -35,25 +38,59 @@ independent_cols =  ['ID_code','target']
 X_train = train.drop(independent_cols, axis=1)
 y_train = train['target']
 
-
 X_train_sub= test.drop('ID_code', axis=1).copy()
-
 
 # =============================================================================
 # Normalizing the Columns
 # =============================================================================
-from sklearn import preprocessing
-
 min_scaler =preprocessing.MinMaxScaler()
 X_scaled = min_scaler.fit_transform(X_train)
 X_train=pd.DataFrame(X_scaled)
-
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X_train, y_train, test_size = 0.1 ,random_state=120)
-
-
 X_scaled_sub = min_scaler.fit_transform(X_train_sub)
 X_train_sub=pd.DataFrame(X_scaled_sub)
+
+# =============================================================================
+# Feature Selection
+# =============================================================================
+KBestModel = SelectKBest(score_func=chi2, k=125)
+KBestModel = KBestModel.fit(X_train, y_train)
+X_train = KBestModel.transform(X_train)
+X_train_sub = KBestModel.transform(X_train_sub)
+
+#To check if the ranks are suitable or not
+colRank = []
+for index,val in enumerate(KBestModel.get_support()):
+    if val:
+        colRank.append(KBestModel.scores_[index])
+
+colRank.sort()
+
+# =============================================================================
+# Splitting the data into training and testing
+# =============================================================================
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X_train, y_train, test_size = 0.18 ,random_state=120)
+
+
+# =============================================================================
+# Oversampling- SMOTE
+# =============================================================================
+print("Number transactions X_train dataset: ", X_train.shape)
+print("Number transactions y_train dataset: ", y_train.shape)
+
+print("Before OverSampling, counts of label '1': {}".format(sum(y_train==1)))
+print("Before OverSampling, counts of label '0': {} \n".format(sum(y_train==0)))
+
+sm = SMOTE(random_state=2)
+X_train_res, y_train_res = sm.fit_sample(X_train, y_train.ravel())
+X_test_res, y_test_res = sm.fit_sample(X_test, y_test.ravel())
+
+print('After OverSampling, the shape of train_X: {}'.format(X_train_res.shape))
+print('After OverSampling, the shape of train_y: {} \n'.format(y_train_res.shape))
+
+print("After OverSampling, counts of label '1': {}".format(sum(y_train_res==1)))
+print("After OverSampling, counts of label '0': {}".format(sum(y_train_res==0)))
+
 # =============================================================================
 # Models for Prediction
 # =============================================================================
@@ -61,84 +98,65 @@ X_train_sub=pd.DataFrame(X_scaled_sub)
 #LOGISTIC REGRESSION
 from sklearn.linear_model import LogisticRegression
 modelLogistic = LogisticRegression()
-
-modelLogistic = modelLogistic.fit(X_train,y_train)
+modelLogistic = modelLogistic.fit(X_train_res,y_train_res)
 
 #Results
 print("Results For Logistic Regression")
-scoreLogistic=modelLogistic.score(X_test, y_test)
+scoreLogistic=modelLogistic.score(X_test_res,y_test_res)
 print("\nScore", scoreLogistic*100)
-
-
 y_Pred = modelLogistic.predict(X_test)
-
 
 
 #DECISION TREE
 from sklearn.tree import DecisionTreeClassifier
 modelDecision = DecisionTreeClassifier(random_state = 0)
-
-modelDecision = modelDecision.fit(X_train , y_train)
-
+modelDecision = modelDecision.fit(X_train_res, y_train_res)
 print("Results For Decision Tree")
-scoreDecision=modelDecision.score(X_test, y_test)
+scoreDecision=modelDecision.score(X_test_res, y_test_res)
 print("\nScore", scoreDecision*100)
-
 y_Pred = modelDecision.predict(X_test)
-
 
 
 #RANDOM FOREST
 from sklearn.ensemble import RandomForestClassifier
-modelRandom = RandomForestClassifier()
-modelRandom = modelRandom.fit(X_train, y_train)
-
+modelRandom = RandomForestClassifier(n_estimators=200, n_jobs=4, class_weight='balanced')
+modelRandom = modelRandom.fit(X_train_res, y_train_res)
 print("Results For Random Forest")
-scoreRandom=modelRandom.score(X_test, y_test)
+scoreRandom=modelRandom.score(X_test_res, y_test_res)
 print("\nScore", scoreRandom*100)
 
-y_Pred = modelRandom.predict(X_test)
-
-
+#Boruta feature elimination
+#boruta_selector = BorutaPy(modelRandom, n_estimators='auto', verbose=2, max_iter=40)
+#boruta_selector.fit(X_train_res, y_train_res)
 
 #XGBoost ...51.3
 import xgboost as xgb
 modelXgb = xgb.XGBClassifier(booster= 'gbtree', objective="binary:logistic", random_state=200)
-
 modelXgb = modelXgb.fit(X_train, y_train)
-
 print("Results For XGBoost")
 scoreXgb=modelXgb.score(X_test, y_test)
 print("\nScore", scoreXgb*100)
-
 y_Pred = modelXgb.predict(X_test)
-
 y_Pred = modelXgb.predict(X_train_sub)
-
 
 #GAUSSIAN NAIVE BAYES
 from sklearn.naive_bayes import GaussianNB
 modelGaussian = GaussianNB()
-
-modelGaussian = modelGaussian.fit(X_train, y_train)
-
+modelGaussian = modelGaussian.fit(X_train_res, y_train_res)
 print("Results For GaussianNB")
-scoreGaussianNB=modelGaussian.score(X_test, y_test)
+scoreGaussianNB=modelGaussian.score(X_test_res, y_test_res)
 print("\nScore", scoreGaussianNB*100)
-
 y_Pred = modelGaussian.predict(X_train_sub)
 
 
 #MULTINOMIAL NAIVE BAYES
 from sklearn.naive_bayes import MultinomialNB
 modelMultinomialNB = MultinomialNB(alpha=1.0, class_prior=None, fit_prior=True)
-
-modelMultinomialNB = modelMultinomialNB.fit(X_train, y_train)
-
+modelMultinomialNB = modelMultinomialNB.fit(X_train_res, y_train_res)
 print("Results For MultinomialNB")
-scoreMultinomialNB=modelMultinomialNB.score(X_test, y_test)
+scoreMultinomialNB=modelMultinomialNB.score(X_test_res, y_test_res)
 print("\nScore", scoreMultinomialNB*100)
-
+y_Pred = modelMultinomialNB.predict(X_train_sub)
 
 #CatBoost...66.8%
 from catboost import CatBoostClassifier, Pool
@@ -155,20 +173,21 @@ modelCatBoost = CatBoostClassifier(loss_function="CrossEntropy",
                                logging_level='Verbose')
 
 modelCatBoost = modelCatBoost.fit(X_train, y_train)
-
 print("Results For CatBoost")
 scoreCatBoost=modelCatBoost.score(X_test, y_test)
 print("\nScore", scoreCatBoost*100)
-
-
 y_Pred = modelCatBoost.predict(X_train_sub)
+
+
 # =============================================================================
 # To Calculate Probability
 # =============================================================================
 probabilityLogistic = modelLogistic.predict_proba(X_test)[::,1]
 
 
-
+# =============================================================================
+# Creating Submission file
+# =============================================================================
 submission = pd.DataFrame({
         "ID_code": test["ID_code"],
         "target": y_Pred
