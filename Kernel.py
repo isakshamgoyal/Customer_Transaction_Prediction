958407dab628b30cfc38d4b8b4dc1d06ca76bb93
spikeeSakshu <spikee.sakshu@gmail.com>
2019-03-10 23:13:06 +0530

    LightGBM

--- Kernel.py
+++ Kernel.py
@@ -70,7 +70,7 @@ colRank.sort()
 # Splitting the data into training and testing
 # =============================================================================
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X_train, y_train, test_size = 0.18 ,random_state=120, stratify=y_train)
+X_train, X_test, y_train, y_test = train_test_split(X_train, y_train, test_size = 0.2 ,random_state=142)
 
 
 # =============================================================================
@@ -122,7 +122,7 @@ y_Pred = modelDecision.predict(X_test)
 
 #RANDOM FOREST
 from sklearn.ensemble import RandomForestClassifier
-modelRandom = RandomForestClassifier(n_estimators=200, n_jobs=4, class_weight='balanced')
+modelRandom = RandomForestClassifier(n_estimators=100, n_jobs=4, class_weight='balanced')
 modelRandom = modelRandom.fit(X_train_res, y_train_res)
 print("Results For Random Forest")
 scoreRandom=modelRandom.score(X_test_res, y_test_res)
@@ -146,7 +146,7 @@ y_Pred = modelXgb.predict(X_train_sub)
 
 #GAUSSIAN NAIVE BAYES
 from sklearn.naive_bayes import GaussianNB
-modelGaussian = GaussianNB()
+modelGaussian = GaussianNB(priors=None, var_smoothing=1e-09)
 modelGaussian = modelGaussian.fit(X_train_res, y_train_res)
 
 print("Results For GaussianNB")
@@ -167,25 +167,124 @@ y_Pred = modelMultinomialNB.predict(X_train_sub)
 
 #CatBoost...66.8%
 from catboost import CatBoostClassifier, Pool
-modelCatBoost = CatBoostClassifier(loss_function="CrossEntropy",
-                               eval_metric="AUC",
-                               learning_rate=0.01,
-                               iterations=400,
-                               random_seed=42,
-                               od_type="Iter",
-                               depth=8,
-                               border_count=32,
-                               early_stopping_rounds=700,
-                               task_type = "GPU",
-                               logging_level='Verbose')
+from sklearn.model_selection import KFold
+from sklearn.metrics import roc_auc_score
+
+modelCatBoost = CatBoostClassifier(loss_function="Logloss",
+                           eval_metric="AUC",
+                           task_type="GPU",
+                           learning_rate=0.01,
+                           iterations=10000,
+                           random_seed=42,
+                           od_type="Iter",
+                           depth=10,
+                           early_stopping_rounds=500
+                          )
+
+#modelCatBoost = CatBoostClassifier(iterations=3000, learning_rate=0.03, objective="Logloss", eval_metric='AUC')
+
+n_split = 5
+kf = KFold(n_splits=n_split, random_state=42, shuffle=True)
+
+y_valid_pred = 0 * y_train
+y_test_pred = 0
+
+for idx, (train_index, valid_index) in enumerate(kf.split(train)):
+    y_train_model, y_valid = y_train.iloc[train_index], y_train.iloc[valid_index]
+    X_train_model, X_valid = X_train.iloc[train_index,:], X_train.iloc[valid_index,:]
+    _train = Pool(X_train_model, label=y_train)
+    _valid = Pool(X_valid, label=y_valid)
+    print( "\nFold ", idx)
+    fit_model = modelCatBoost.fit(_train,
+                          eval_set=_valid,
+                          use_best_model=True,
+                          verbose=200,
+                          plot=True
+                         )
+    pred = fit_model.predict_proba(X_valid)[:,1]
+    print( "  auc = ", roc_auc_score(y_valid, pred) )
+    y_valid_pred.iloc[valid_index] = pred
+    y_test_pred += fit_model.predict_proba(test)[:,1]
+    
+y_test_pred /= n_split
+
 
 modelCatBoost = modelCatBoost.fit(X_train, y_train)
 print("Results For CatBoost")
 scoreCatBoost=modelCatBoost.score(X_test, y_test)
 print("\nScore", scoreCatBoost*100)
-y_Pred = modelCatBoost.predict(X_train_sub)
+y_Pred_Cat = modelCatBoost.predict(X_train_sub)
 
 
+import lightgbm as lgb
+from sklearn.model_selection import StratifiedKFold
+
+params = {
+        'num_leaves': 8,
+         'min_data_in_leaf': 42,
+         'objective': 'binary',
+         'max_depth': 16,
+         'learning_rate': 0.0123,
+         'boosting': 'gbdt',
+         'bagging_freq': 5,
+         'bagging_fraction': 0.8,
+         'feature_fraction': 0.8201,
+         'bagging_seed': 11,
+         'reg_alpha': 1.728910519108444,
+         'reg_lambda': 4.9847051755586085,
+         'random_state': 42,
+         'metric': 'auc',
+         'verbosity': -1,
+         'subsample': 0.81,
+         'min_gain_to_split': 0.01077313523861969,
+         'min_child_weight': 19.428902804238373,
+         'num_threads': 4
+    }
+
+param = {
+        'num_leaves': 10,
+        'max_bin': 119,
+        'min_data_in_leaf': 11,
+        'learning_rate': 0.02,
+        'min_sum_hessian_in_leaf': 0.00245,
+        'bagging_fraction': 1.0, 
+        'bagging_freq': 5, 
+        'feature_fraction': 0.05,
+        'lambda_l1': 4.972,
+        'lambda_l2': 2.276,
+        'min_gain_to_split': 0.65,
+        'max_depth': 14,
+        'save_binary': True,
+        'seed': 1337,
+        'feature_fraction_seed': 1337,
+        'bagging_seed': 1337,
+        'drop_seed': 1337,
+        'data_random_seed': 1337,
+        'objective': 'binary',
+        'boosting_type': 'gbdt',
+        'verbose': 1,
+        'metric': 'auc',
+        'is_unbalance': True,
+        'boost_from_average': False}
+
+n_fold = 5
+folds= StratifiedKFold(n_splits=n_fold, shuffle=True, random_state=42)
+
+y_Pred = np.zeros(len(X_train_sub))
+
+
+for fold_n, (train_index, valid_index) in enumerate(folds.split(X_train, y_train)):
+    print('Fold', fold_n)
+    X_training, X_validation = X_train.iloc[train_index], X_train.iloc[valid_index]
+    y_training, y_validation = y_train.iloc[train_index], y_train.iloc[valid_index]
+    
+    training_data = lgb.Dataset(X_training, label=y_training)
+    validation_data = lgb.Dataset(X_validation, label=y_validation)
+    
+modelLgb = lgb.train(params,training_data,num_boost_round=20000,
+                    valid_sets = [training_data, validation_data],verbose_eval=300,early_stopping_rounds = 200)
+
+y_Pred += modelLgb.predict(X_train_sub, num_iteration=modelLgb.best_iteration)/5
 # =============================================================================
 # To Calculate Probability
 # =============================================================================
@@ -197,7 +296,7 @@ probabilityLogistic = modelLogistic.predict_proba(X_test)[::,1]
 # =============================================================================
 submission = pd.DataFrame({
         "ID_code": test["ID_code"],
-        "target": y_Pred
+        "target": (y_Pred + y_Pred_Cat)/2
     })
 submission.to_csv('DataBase/submission.csv', index=False)
 
